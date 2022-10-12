c2b93d247767ddb601b7ab08e0d3ed39e046fb0f
Wongsathorn632115035 <wongsathorn_w@cmu.ac.th>
2022-10-12 15:30:16 +0700

    2.5

--- src/views/EventForm.vue
+++ src/views/EventForm.vue
@@ -53,11 +53,20 @@ export default {
         description: '',
         location: '',
         organizer: { id: '', name: '' }
-      }
+      },
+      files: []
     }
   },
   methods: {
     saveEvent() {
+      Promise.all(
+        this.files.map((file) => {
+          return EventService.uploadFile(file)
+        })
+      ).then((response) => {
+        console.log(response)
+        console.log('finish upload file')
+      })
       EventService.saveEvent(this.event)
         .then((response) => {
           console.log(response)
@@ -75,8 +84,9 @@ export default {
           this.$router.push('NetworkError')
         })
     },
-    handleImage(files) {
+    handleImages(files) {
       console.log(files)
+      this.files = files
     }
   }
 }
